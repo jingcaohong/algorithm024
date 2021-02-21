f3b09e831c7439b1d049fe822f418aa522cb20b5
hongjingcao <jingcaohong@gmail.com>
2021-02-21 22:21:18 +0800

    卧槽，只要一天不做就会一整周都不做。。。别想着拖延了。

--- Week_03/README.md
+++ Week_03/README.md
@@ -1,1 +1,70 @@
 学习笔记
+
+#### 递归 Recursion
+递归代码模板
+
+```
+// Java
+public void recur(int level, int param) { 
+  // terminator 递归终止点
+  if (level > MAX_LEVEL) { 
+    // process result 
+    return; 
+  }
+  // process current logic 
+  process(level, param); 
+  // drill down 
+  recur( level: level + 1, newParam); 
+  // restore current status 
+}
+```
+递归：
+1. 本质是循环
+2. 通过函数体来进行的循环
+
+注意点：
+1. 抵制人肉递归
+2. 找最近重复性
+3. 数学归纳法
+
+
+
+
+
+
+
+---
+树的面试题解法一般都是递归  
+BST的中序遍历是有序递增的  
+char[] chars = s.toCharArray();  
+f (chars[i] == ' ')
+
+
+
+
+分治和回溯其实就是特殊的递归  
+的
+
+重复性
+
+分治代码模板
+
+```python
+def divide_conquer(problem, p1, p2, ...):
+    # recursion terminator
+    if problem is None:
+        print_result
+        return
+    # prepare data
+    data = prepare_data(problem)
+    subproblems = split_problem(problem, data)
+    # conquer subproblems
+    subresult1 = self.divide_conquer(subproblems[0], p1, ...)
+    subresult2 = self.divide_conquer(subproblems[1], p1, ...)
+    ...
+    # process and generate the final result
+    result = process_result(subresult1, subresult2, ...)
+    
+```
+
+数组是引用传递的，注意分支污染
---
+++ Week_03/[46]全排列Permutations.java
@@ -0,0 +1,57 @@
+  //给定一个 没有重复 数字的序列，返回其所有可能的全排列。 
+//
+// 示例: 
+//
+// 输入: [1,2,3]
+//输出:
+//[
+//  [1,2,3],
+//  [1,3,2],
+//  [2,1,3],
+//  [2,3,1],
+//  [3,1,2],
+//  [3,2,1]
+//] 
+// Related Topics 回溯算法 
+// 👍 1142 👎 0
+
+  
+  package com.shuzijun.leetcode.editor.en;
+  public class Permutations{
+      public static void main(String[] args) {
+           Solution solution = new Permutations().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+          public List<List<Integer>> permute(int[] nums) {
+              List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+              List<Integer> output = new ArrayList<Integer>();
+              for (int num : nums) {
+                  output.add(num);
+              }
+
+              int n = nums.length;
+              backtrack(n, output, res, 0);
+              return res;
+          }
+
+          public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+              // 所有数都填完了
+              if (first == n) {
+                  res.add(new ArrayList<Integer>(output));
+              }
+              for (int i = first; i < n; i++) {
+                  // 动态维护数组
+                  Collections.swap(output, first, i);
+                  // 继续递归填下一个数
+                  backtrack(n, output, res, first + 1);
+                  // 撤销操作
+                  Collections.swap(output, first, i);
+              }
+          }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
